72be9e68d0c4c1fe701a7b517ad6515d1b469f7b
Dhruv Bavadiya <dhruvbavadiya@Dhruvs-MacBook-Air.local>
2024-05-17 17:31:41 +0530

    added temp log file

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ require('dotenv').config();
 const signalRoutes = require('./Routes/signalRoutes');
 const app = express();
 const server = http.createServer(app);
+const fs = require('fs')
 
 // Use cors middleware
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const ip = req.ip;
+  const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''); // Format timestamp
+  const userAgent = req.get('User-Agent');
+  const url = req.originalUrl;
+  const method = req.method;
+
+  const logMessage = `${timestamp}\t${ip}\t${method}\t${url}\tUser-Agent: ${userAgent}\n`;
+
+  fs.appendFile('access_log.txt', logMessage, (err) => {
+      if (err) {
+          console.error('Error logging request:', err);
+      }
+  });
+
+  next();
+});
+
+
+
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
